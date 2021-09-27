23690dffc3c5b03db3873dd2e318e291fd616f41
wiswisbus <39900094+wiswisbus@users.noreply.github.com>
2021-09-27 23:45:58 +0900

    test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     st.set_page_config(page_title="Supertone Web App v0.01")
-    st.text('Test')
+    st.text('Test2')
 
 if __name__ == "__main__":
     main()
