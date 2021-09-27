f0eed005df796935249af81327289c4bcf15e625
wiswisbus <39900094+wiswisbus@users.noreply.github.com>
2021-09-27 16:47:20 +0900

    change dockerfile port

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def main():
-    st.set_page_config(page_title="Supertone Web App v0.1")
+    st.set_page_config(page_title="Supertone Web App v0.01")
 
 if __name__ == "__main__":
     main()
